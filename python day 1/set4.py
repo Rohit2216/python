6935904ecc9167698f1bf50920d221aa74458dab
Rohit2216 <chauhanrohit716@gmail.com>
2023-08-05 16:34:35 +0530

    set4 done

--- python day 1/set4.py
+++ python day 1/set4.py
@@ -593,6 +593,21 @@ print(trie.search("app"))       # Output: False (searching for a full word, "app
 
 
 
+def max_subarray_sum(nums):
+    max_sum = current_sum = nums[0]
+
+    for num in nums[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+# Example usage:
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+max_sum = max_subarray_sum(nums)
+print("Maximum Subarray Sum:", max_sum)  # Output: 6
+
+
 
 
 
