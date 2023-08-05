00850150a5989f4f3a3def8c3041534b75e18863
Rohit2216 <chauhanrohit716@gmail.com>
2023-08-05 16:33:59 +0530

    set4 done

--- python day 1/set4.py
+++ python day 1/set4.py
@@ -541,6 +541,47 @@ print(is_palindrome(linked_list))  # Output: False
 
 
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end_of_word
+
+    def startsWith(self, prefix):
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+# Example usage:
+trie = Trie()
+trie.insert("apple")
+print(trie.search("apple"))     # Output: True
+print(trie.startsWith("app"))   # Output: True
+print(trie.search("app"))       # Output: False (searching for a full word, "app" is not in the Trie)
+
+
 
 
 
