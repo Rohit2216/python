6fe3ba414c7cc665ba8a21a205945944db5904cd
Rohit2216 <chauhanrohit716@gmail.com>
2023-08-05 10:53:49 +0530

    2 program done

---
+++ firstday.py
@@ -0,0 +1,30 @@
+# 1. **Hello, World!**: Write a Python program that prints "Hello, World!" to the console.
+#     - *Input*: None
+#     - *Output*: "Hello, World!"
+
+print("Hello, world")
+
+
+# 2. **Data Type Play**: Create variables of each data type (integer, float, string, boolean, list, tuple, dictionary, set) and print their types and values.
+#     - *Input*: None
+#     # - *Output*: "Type of variable1: <class 'int'>, value: 10..."
+
+
+int_pyt=45
+fl_pyt=3.14
+str_pyt="Hello Python"
+bool_pyt=True
+list_pyt=[1,2,3,4]
+tuple_pyt=(1,2,"hello")
+dictionary_pyt={"name":"Rohit","lastname":"Chauhan"}
+set_pyt=(1,2,3,4)
+
+
+print("Type of int_pyt:", type(int_pyt), ", value:", int_pyt)
+print("Type of fl_pyt:", type(fl_pyt), ", value:", fl_pyt)
+print("Type of bool_pyt:", type(bool_pyt), ", value:", bool_pyt)
+print("Type of str_pyt:", type(str_pyt), ", value:", str_pyt)
+print("Type of list_pyt:", type(list_pyt), ", value:", list_pyt)
+print("Type of tuple_pyt:", type(tuple_pyt), ", value:", tuple_pyt)
+print("Type of dictionary_pyt:", type(dictionary_pyt), ", value:", dictionary_pyt)
+print("Type of set_pyt:", type(set_pyt), ", value:", set_pyt)
