7fa428193b8b2b151e3dd9fe048908b20f502909
Rohit2216 <chauhanrohit716@gmail.com>
2023-08-05 13:39:27 +0530

    set3 done

--- python day 1/set3.py
+++ python day 1/set3.py
@@ -77,3 +77,129 @@ nums = [2, 7, 11, 15]
 target = 9
 result = find_two_sum(nums, target)
 print(result)  # Output: [0, 1] (because nums[0] + nums[1] = 2 + 7 = 9)
+
+
+
+# 1. **Palindrome Check**: Write a Python function that checks whether a given word or phrase is a palindrome.
+#     - *Input*: "madam"
+#     - *Output*: "The word madam is a palindrome."
+
+
+
+def is_palindrome(s):
+    # Remove all non-alphanumeric characters and convert the string to lowercase
+    cleaned_s = ''.join(char.lower() for char in s if char.isalnum())
+
+    # Compare the cleaned string with its reverse
+    return cleaned_s == cleaned_s[::-1]
+
+# Example usage:
+string1 = "Able was I, ere I saw Elba."
+string2 = "racecar"
+string3 = "hello world"
+
+print(is_palindrome(string1))  # Output: True
+print(is_palindrome(string2))  # Output: True
+print(is_palindrome(string3))  # Output: False
+
+
+
+
+# 1. **Selection Sort**: Implement the selection sort algorithm in Python.
+#     - *Input*: [64, 25, 12, 22, 11]
+#     - *Output*: "[11, 12, 22, 25, 64]"
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        # Swap the minimum element with the first unsorted element
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+# Example usage:
+arr = [64, 25, 12, 22, 11]
+selection_sort(arr)
+print("Sorted array:", arr)
+
+# Output: Sorted array: [11, 12, 22, 25, 64]
+
+# 1. **Implement Stack using Queue**: Use Python's queue data structure to implement a stack.
+#     - *Input*: push(1), push(2), pop(), push(3), pop(), pop()
+#     - *Output*: "1, None, 3, None, None"
+
+from queue import LifoQueue
+
+class Stack:
+    def __init__(self):
+        self.stack = LifoQueue()
+
+    def push(self, item):
+        self.stack.put(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.get()
+
+    def is_empty(self):
+        return self.stack.empty()
+
+# Example usage:
+stack = Stack()
+
+stack.push(1)
+stack.push(2)
+print(stack.pop())  # Output: 2
+
+stack.push(3)
+print(stack.pop())  # Output: 3
+print(stack.pop())  # Output: 1
+
+
+
+# 1. **FizzBuzz**: Write a Python program that prints the numbers from 1 to 100, but for multiples of three, print "Fizz" instead of the number, for multiples of five, print "Buzz", and for multiples of both three and five, print "FizzBuzz".
+#     - *Input*: None
+#     - *Output*: "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 
+# 16,..."
+
+
+
+
+def fizzBuzz(num):
+    for i in range(1, num + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Example usage:
+fizzBuzz(15)
+
+
+
+
+
+
+# 1. **File I/O**: Write a Python program that reads a file, counts the number of words, and writes the count to a new file.
+#     - *Input*: A text file named "input.txt" with the content "Hello world"
+#     - *Output*: A text file named "output.txt" with the content "Number of words: 2"
+
+
+
+
+
+
+
+
+
+
+# 2. **Exception Handling**: Write a Python function that takes two numbers as inputs and returns their division, handling any potential exceptions (like division by zero).
+#     - *Input*: 5, 0
+#     - *Output*: "Cannot divide by zero."
